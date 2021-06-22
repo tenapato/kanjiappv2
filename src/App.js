773ffda42a7e2c6d1f9c181f8bce98654ff1d193
tenapato <tenapatricio@gmail.com>
2021-06-21 18:53:01 -0700

    Initial commit, and sample flashcards

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import FlashcardList from './FlashcardList';
 
 function App() {
+  const [flashcards, setFlashcards] = useState(SAMPLE_FLASHCARDS);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   
+   <FlashcardList flashcards={flashcards}/>
   );
 }
+
+const SAMPLE_FLASHCARDS = [
+
+  {
+    id: 1,
+    kanji: '語',
+    kunyomi: ['かたる','かたらう'],
+    onyomi: ['ゴ'],
+    stroke_count: 14,
+    meanings: ["word","speech","language"],
+  
+  },
+  {
+    id: 2,
+    kanji: '日本人',
+    kunyomi: ['かたる','かたらう'],
+    onyomi: ['ゴ'],
+    stroke_count: 14,
+    meanings: ["word","speech","language"],
+  
+  },
+
+
+]
+
+
+
+
 
 export default App;
